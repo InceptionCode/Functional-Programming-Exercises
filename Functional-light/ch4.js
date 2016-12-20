9c985531f4bcd3a58a91ba95a3591f2ab0367dff
Darrell Washington <darrellwjr.com@gmail.com>
2016-12-20 17:38:35 -0500

    Finish Chapter 4 w/ additional exercises
    Added composing exercises after the Candy Factory example

--- Functional-light/ch4.js
+++ Functional-light/ch4.js
@@ -69,3 +69,59 @@ function composing ( fn2, fn1 ) {
 const composedMachine = composing( cuttingCandy, coolingChocolate );
 
 composedMachine( candyBatch3 );
+
+/* Composing our own exercise */
+
+//This is the actually compose function used in the book
+
+function compose ( ...fns ) {
+
+  return fns.reverse().reduce( function reducer ( fn1,fn2 ) {
+
+      return function composed ( ...args ) {
+
+          return fn2( fn1( ...args ) );
+
+        };
+
+    } );
+
+}
+
+// Exercise 1: A list of names has to be broken up and every name has to be counted for.
+// Once the names are counted a new object has to be created with the amount of names in the list.
+// Finally the new object will be pushed to a server where it will be stored on a database.
+// Compose the seperate task of the code.
+
+const nameObjectRequest = {
+  method: 'POST',
+  url: 'http://some.api'
+},
+  listA = [ 'James', 'Louis', 'Adamn', 'Rachael', 'Rebecca' ];
+
+function returnNameProperties ( ListOfNames, nameObject ) {
+
+  let amountOfNames = ListOfNames.length;
+  return [ nameObject, amountOfNames ];
+
+}
+
+function createObjectData ( [ nameObject, amountOfNames ] ) {
+
+  return Object.assign( {}, nameObject, { data: { amount: amountOfNames } } );
+
+}
+
+function sendDataToDatabase ( nameObject ) {
+
+  setTimeout ( ()=> {
+
+    /* NOTE: turn this return statement into a console.log */
+    return 'Data has been sent', nameObject;
+
+  }, 1000 );
+
+}
+
+const composedNameOperation = compose( sendDataToDatabase, createObjectData, returnNameProperties );
+composedNameOperation( listA, nameObjectRequest );
