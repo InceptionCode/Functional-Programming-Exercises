6e61ea7ba0ecfb88972abaaf004456b44021546c
Darrell Washington <darrellwjr.com@gmail.com>
2017-01-13 18:15:05 -0500

    Finished chp8 Exercises for Mostly-Adequate

--- Mostly-Adequate/chp8.js
+++ Mostly-Adequate/chp8.js
@@ -1,24 +1,30 @@
 const R = require('ramda');
 
 //----Creating our very own Functors -----
-//StrinFunctor :: ( string, f ) -> f([string]) -> [string] 
-const StringFunctor = ( value, fn ) => {
-	let chars = value.split("");
-	let fromChar = char => String.fromCharCode(fn(char.charCodeAt(0) ) );
-	return R.map( fromChar, chars );
-};	
+
+const StringFunctor = function ( value )  { this.chars = value.split(''); };	
+StringFunctor.of = value => new StringFunctor(value);
+//StrinFunctor.map :: f -> map( f, [string] ) -> [string]
+StringFunctor.prototype.map = function (f) {
+	
+	let fromChar = char => String.fromCharCode(f(char.charCodeAt(0) ) );
+	return StringFunctor.of( R.map( fromChar, this.chars ) );
+};
 const plus1 = value => value + 1;
 const convertToNum = str => parseInt(str);
-//NOTE: console.log(StringFunctor("ABC",plus1));
+/*NOTE: console.log(StringFunctor.of('ABC').map( plus1 ) ); */
 
 //NumberFunctor :: number  -> string -> Functor -> [string] -> [number];
-const NumberFunctor = value => {
-      let numbers = value.toString();
-      let incrementedNumbers = StringFunctor( numbers, plus1 );
-      return R.map( convertToNum, incrementedNumbers );
-};      
+const NumberFunctor = function ( value ) { this.numbers = value; };
+NumberFunctor.of = value => new NumberFunctor( value );
+NumberFunctor.prototype.map = function ( f ) {
+      let newNumbers = R.map( convertToNum, this.numbers.toString().split('') ); 
+      let finalNumbers = R.map( f, newNumbers );
+      return NumberFunctor.of(finalNumbers);
 
-//NOTE: console.log(NumberFunctor(123) );
+};  
+
+/* NOTE: console.log( NumberFunctor.of( 123 ).map( plus1 ) ); */
 
 //Identity Functor 
 const Identity = function(x) { this._value = x; };
@@ -27,8 +33,12 @@ Identity.of = x => new Identity(x);
 //Maybe Functor 
 const Maybe = function(x) { this._value = x; };
 Maybe.of = x => new Maybe(x);
-Maybe.prototype.isNothing = () => ( this._value === null || this._value === undefined );
-Maybe.prototype.map = f => ( this.isNothing() ) ? Maybe.of(null) : Maybe.of( f(this._value) );
+Maybe.prototype.isNothing = function () {
+	return ( this._value === null || this._value === undefined );
+};
+Maybe.prototype.map = function(f) {
+       return this.isNothing() ? Maybe.of(null) : Maybe.of( f(this._value) );
+};
 
 //Either Functor
 const Either = R.curry( (f, g, e )=> {
@@ -50,11 +60,11 @@ Right.of = x => new Right(x);
 Left.prototype.map = f => this;
 Right.prototype.map = f => Right.of( f(this._value) );
 
-//incrementOrPrint : : value -> Either( String, Number )
+//incrementOrPrint : : value -> Either( Number,String )
 const incrementOrPrint = x => (typeof x === "number") ?  Right.of( x ) : Left.of( `What did you just give me? A ${x} is not a number`);
 //NOTE: Pass the increment... function any value to see Either at work. Also, you can run Either by it's self or console.log( Either ).
 //The choice is yours.
-//console.log( Either( console.log, NumberFunctor, incrementOrPrint(22) ) );
+//console.log( Either( console.log, plus1, incrementOrPrint(22) ) );
 
 // Exercise 1
 // ==========
@@ -66,29 +76,34 @@ const  ex1 = Functor =>  R.map( Adder, Functor._value );
 
 //REVIEW: Will review this exercise at a later time. Feel free to run this line of code ---> console.log( ex1(Functor));
 
-//TODO
+
 // Exercise 2
 // ==========
 // Use _.head to get the first element of the list.
 var xs = Identity.of(['do', 'ray', 'me', 'fa', 'so', 'la', 'ti', 'do']);
 
-var ex2 = undefined;
-
+const ex2 = R.head(xs._value);
+//NOTE: console.log(ex2);
 
 
 // Exercise 3
 // ==========
 // Use safeProp and _.head to find the first initial of the user.
-var safeProp = _.curry(function(x, o) {
+var safeProp = R.curry(function(x, o) {
 	  return Maybe.of(o[x]);
 });
+//console.log( safeProp( 0, Functor._value ) );
 
 var user = {
 	  id: 2,
-	    name: 'Albert',
+	  name: 'Albert',
+	  active: false
 };
 
-var ex3 = undefined;
+const user_name = safeProp( "name", user )._value;
+
+var ex3 = R.head(user_name);
+//console.log(ex3);
 
 
 // Exercise 4
@@ -97,48 +112,36 @@ var ex3 = undefined;
 
 var ex4 = function(n) {
 	  if (n) {
-		      return parseInt(n);
-		        }
+	     return parseInt(n);
+        }
 };
 
-var ex4 = undefined;
+//ex4_new : : f, string -> f(string) -> Maybe(_, number )
+var ex4_new = ( f,n ) =>{
 
-
-
-// Exercise 5
-// ==========
-// Write a function that will getPost then toUpperCase the post's title.
-
-// getPost :: Int -> Future({id: Int, title: String})
-var getPost = function(i) {
-	  return new Task(function(rej, res) {
-		      setTimeout(function() {
-			            res({
-					            id: i,
-						            title: 'Love them futures',
-							          });
-				        }, 300);
-		        });
+	return Maybe.of(n).map(f);	
 };
 
-var ex5 = undefined;
-
-
+//NOTE: console.log( ex4_new(parseInt, "3") );
 
 // Exercise 6
 // ==========
 // Write a function that uses checkActive() and showWelcome() to grant access
 // or return the error.
 
-var showWelcome = _.compose(_.concat( "Welcome "), _.prop('name'));
+var showWelcome = R.compose(R.concat( "Welcome "), R.prop('name'));
 
 var checkActive = function(user) {
 	  return user.active ? Right.of(user) : Left.of('Your account is not active');
 };
 
-var ex6 = undefined;
-
-
+var ex6 = Either( Identity.of, showWelcome );
+const user2 = {
+	id: 2,
+	name: "Darrell-I-C",
+	active: true
+};
+//NOTE:console.log( ex6( checkActive(user2) ) ); switch from user2 -> user in order to see a non active account 
 
 // Exercise 7
 // ==========
@@ -146,9 +149,10 @@ var ex6 = undefined;
 // Right(x) if it is greater than 3 and Left("You need > 3") otherwise.
 
 var ex7 = function(x) {
-	  return undefined; // <--- write me. (don't be pointfree)
-};
 
+	  return (x.length > 3 ) ?  Right.of( x ) : Left.of( "You need > 3" ); 
+};
+// NOTE: console.log( ex7("four") ); change the input's length in order to see a Left.of result.
 
 
 // Exercise 8
@@ -158,14 +162,14 @@ var ex7 = function(x) {
 // the same type.
 
 var save = function(x) {
-	  return new IO(function() {
-		      console.log('SAVED USER!');
-		          return x + '-saved';
-			    });
+	  return (function() {
+		   console.log('SAVED USER!');
+		   return x + '-saved';
+          })();
 };
 
-var ex8 = undefined;
-
+var ex8 = R.compose( Either( console.log, save ), ex7 );
+//NOTE: console.log( ex8( "user" ) ); Change the length you pass through in order to recieve the error message.
 
 
 
